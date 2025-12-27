0e0c6480754d6fffd68ad65410473f63e6e84c1f
GanstaZ <33817937+GanstaZ@users.noreply.github.com>
2025-12-28 00:52:22 +0200

    Update area controller

--- src/controller/admin/index_controller.php
+++ src/controller/admin/index_controller.php
@@ -11,9 +11,7 @@
 namespace baihu\baihu\src\controller\admin;
 
 // phpcs:disable
-// @codingStandardsIgnoreStart
 use baihu\baihu\src\auth\attribute\is_granted as isGranted;
-// @codingStandardsIgnoreEnd
 // phpcs:enable
 
 use baihu\baihu\src\controller\abstract_controller;
@@ -25,15 +23,13 @@ class index_controller extends abstract_controller
 	#[isGranted('ADMIN', 'a_board', 'AREA_NO_ADMIN', 403)]
 	public function index(): Response
 	{
-		$controller_helper = $this->get_controller_helper();
-
-		$controller_helper->get_template()->assign_vars([
+		$this->template->assign_vars([
 			'VERSION'		=> core::VERSION,
 			'STYLE'			=> core::STYLE,
 			'PHP_VERSION'	=> PHP_VERSION,
 			'BOARD_VERSION'	=> PHPBB_VERSION,
 		]);
 
-		return $controller_helper->render('admin/index.twig', $controller_helper->get_language()->lang('AREA_MAIN_PAGE'));
+		return $this->render('admin/index.twig', $this->language->lang('AREA_MAIN_PAGE'));
 	}
 }
