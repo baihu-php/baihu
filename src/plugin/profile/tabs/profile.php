0127c6dbb5bbd32ca5377a97e76a272a079c78eb
GanstaZ <33817937+GanstaZ@users.noreply.github.com>
2026-01-08 19:34:45 +0200

    Remove unused data

--- src/plugin/profile/tabs/profile.php
+++ src/plugin/profile/tabs/profile.php
@@ -67,107 +67,6 @@ final class profile extends base
 
 		$user_id = (int) $member['user_id'];
 
-		// Get group memberships
-		// Also get visiting user's groups to determine hidden group memberships if necessary.
-		$auth_hidden_groups = $user_id === (int) $user->data['user_id'] || $auth->acl_gets('a_group', 'a_groupadd', 'a_groupdel');
-		$sql_uid_ary = ($auth_hidden_groups) ? [$user_id] : [$user_id, (int) $user->data['user_id']];
-
-		// Do the SQL thang
-		$sql_ary = [
-			'SELECT'	=> 'g.group_id, g.group_name, g.group_type, ug.user_id',
-
-			'FROM'		=> [
-				GROUPS_TABLE => 'g',
-			],
-
-			'LEFT_JOIN' => [
-				[
-					'FROM' => [USER_GROUP_TABLE => 'ug'],
-					'ON'   => 'g.group_id = ug.group_id',
-				],
-			],
-
-			'WHERE'		=> $this->db->sql_in_set('ug.user_id', $sql_uid_ary) . '
-				AND ug.user_pending = 0',
-		];
-
-		/**
-		* Modify the query used to get the group data
-		*
-		* @event core.modify_memberlist_viewprofile_group_sql
-		* @var array	sql_ary			Array containing the query
-		* @since 3.2.6-RC1
-		*/
-		$vars = ['sql_ary',];
-		extract($this->dispatcher->trigger_event('core.modify_memberlist_viewprofile_group_sql', compact($vars)));
-
-		$result = $this->db->sql_query($this->db->sql_build_query('SELECT', $sql_ary));
-
-		// Divide data into profile data and current user data
-		$profile_groups = $user_groups = [];
-		while ($row = $this->db->sql_fetchrow($result))
-		{
-			$row['user_id'] = (int) $row['user_id'];
-			$row['group_id'] = (int) $row['group_id'];
-
-			if ($row['user_id'] == $user_id)
-			{
-				$profile_groups[] = $row;
-			}
-			else
-			{
-				$user_groups[$row['group_id']] = $row['group_id'];
-			}
-		}
-		$this->db->sql_freeresult($result);
-
-		// Filter out hidden groups and sort groups by name
-		$group_data = $group_sort = [];
-		foreach ($profile_groups as $row)
-		{
-			if (!$auth_hidden_groups && $row['group_type'] == GROUP_HIDDEN && !isset($user_groups[$row['group_id']]))
-			{
-				// Skip over hidden groups the user cannot see
-				continue;
-			}
-
-			$row['group_name'] = $this->get_group()->get_name($row['group_name']);
-
-			$group_sort[$row['group_id']] = utf8_clean_string($row['group_name']);
-			$group_data[$row['group_id']] = $row;
-		}
-		unset($profile_groups);
-		unset($user_groups);
-		asort($group_sort);
-
-		/**
-		* Modify group data before options is created and data is unset
-		*
-		* @event core.modify_memberlist_viewprofile_group_data
-		* @var array	group_data			Array containing the group data
-		* @var array	group_sort			Array containing the sorted group data
-		* @since 3.2.6-RC1
-		*/
-		$vars = ['group_data', 'group_sort',];
-		extract($this->dispatcher->trigger_event('core.modify_memberlist_viewprofile_group_data', compact($vars)));
-
-		$group_current = '';
-		$group_options = [];
-		foreach ($group_sort as $group_id => $null)
-		{
-			$row = $group_data[$group_id];
-
-			if ($row['group_id'] == $member['group_id'])
-			{
-				$group_current = $row['group_id'];
-			}
-
-			$group_options[$row['group_id']] = $row['group_name'];
-		}
-
-		unset($group_data);
-		unset($group_sort);
-
 		// What colour is the zebra
 		$sql = 'SELECT friend, foe
 			FROM ' . ZEBRA_TABLE . "
@@ -207,15 +106,8 @@ final class profile extends base
 		}
 
 		// Do the relevant calculations
-		$memberdays = max(1, round((time() - $member['user_regdate']) / 86400));
-		$posts_per_day = $member['user_posts'] / $memberdays;
 		$percentage = ($this->config['num_posts']) ? min(100, ($member['user_posts'] / $this->config['num_posts']) * 100) : 0;
 
-		if ($member['user_sig'])
-		{
-			$parse_flags = ($member['user_sig_bbcode_bitfield'] ? OPTION_FLAG_BBCODE : 0) | OPTION_FLAG_SMILIES;
-			$member['user_sig'] = generate_text_for_display($member['user_sig'], $member['user_sig_bbcode_uid'], $member['user_sig_bbcode_bitfield'], $parse_flags, true);
-		}
 
 		// We need to check if the modules 'zebra', 'notes' and 'warn' are accessible to decide if we can display appropriate links
 		$zebra_enabled = $friends_enabled = $foes_enabled = $user_notes_enabled = $warn_user_enabled = false;
@@ -287,16 +179,10 @@ final class profile extends base
 		// Define the main array of vars to assign to memberlist_view.html
 		$template_ary = [
 			'L_POSTS_IN_QUEUE'			=> $this->language->lang('NUM_POSTS_IN_QUEUE', $member['posts_in_queue']),
-
-			'POSTS_DAY'					=> $this->language->lang('POST_DAY', $posts_per_day),
-			'POSTS_PCT'					=> $this->language->lang('POST_PCT', $percentage),
-
-			'SIGNATURE'					=> $member['user_sig'],
 			'POSTS_IN_QUEUE'			=> $member['posts_in_queue'],
 
-			'S_PROFILE_ACTION'			=> append_sid("{$this->root_path}memberlist.$this->php_ext", 'mode=group'),
-			'S_GROUP_CURRENT'			=> $group_current,
-			'S_GROUP_OPTIONS'			=> $group_options,
+			'POSTS_PCT'					=> $this->language->lang('POST_PCT', $percentage),
+
 			'S_CUSTOM_FIELDS'			=> isset($profile_fields['row']) && count($profile_fields['row']),
 
 			'U_USER_ADMIN'				=> ($auth->acl_get('a_user')) ? append_sid(generate_board_url() . "/{$this->admin_path}index.$this->php_ext", 'i=users&amp;mode=overview&amp;u=' . $user_id, true, $user->session_id) : '',
