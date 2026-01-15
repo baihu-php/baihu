d17db04215a1ba02299d603088792bcf85cebcf4
GanstaZ <33817937+GanstaZ@users.noreply.github.com>
2026-01-15 19:02:55 +0200

    Add extension and minor changes

---
+++ src/twig/extension/rank.php
@@ -0,0 +1,50 @@
+<?php
+/**
+*
+* An extension for the phpBB Forum Software package.
+*
+* @copyright (c) GanstaZ, https://www.github.com/GanstaZ/
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+namespace baihu\baihu\src\twig\extension;
+
+use phpbb\template\twig\environment;
+use Twig\Error\Error;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class rank extends AbstractExtension
+{
+	/**
+	 * Returns a list of functions to add to the existing list.
+	 *
+	 * @return TwigFunction[]			Array of twig functions
+	 */
+	public function getFunctions(): array
+	{
+		return [
+			new TwigFunction('baihu_rank', [$this, 'rank'], ['needs_environment' => true]),
+		];
+	}
+
+	public function rank(environment $environment, array $data): string
+	{
+		$path = $data['ext_name'] ?? 'baihu_baihu';
+		// $rank = $data['rank_special'] ? strtolower($data['rank_title']) : 'default';
+		$rank = 'default';
+
+		try
+		{
+			return $environment->render("@{$path}/macros/rank/{$rank}.twig", [
+				'COLOR' => $data['rank_color'],
+				'TITLE' => $data['rank_title'],
+			]);
+		}
+		catch (Error $e)
+		{
+			return '';
+		}
+	}
+}
